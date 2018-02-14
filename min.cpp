63f979dd3ab8f988ec8940804488e2aae18311e1
Ranzzy <36180889+Ranzzy@users.noreply.github.com>
2018-02-14 16:46:02 +0700

    Add files via upload

---
+++ min.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cstdio>
+using namespace std;
+
+int vector[5]={2,45,34,8,15};
+int main()
+{
+    int i;
+    int min=vector[0];
+    for (i=1; i<5; i++) {
+        if (min>vector[i]) min=vector[i];
+    }
+    printf ("min=%d\n", min);
+}
